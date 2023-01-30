9d23b45de206d8afa2b8294f12c6fc3f20e7f591
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2023-01-29 23:42:07 -0300

    using params and shell

--- myapp/views.py
+++ myapp/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def hello(request):
-    return HttpResponse('<h1>Hello world!</h1>')
+def index(request):
+    return HttpResponse("Index page")
+
+def hello(request, id):
+    result = id + 2 * 5
+    
+    return HttpResponse(f'<h1>Hello {result}</h1>')
 
 def about(request):
     return HttpResponse('<h2>About</h2>')
