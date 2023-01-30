4c8fa3017d32524b2b04cc4281711d9ed5a0ba5d
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2023-01-30 20:45:30 -0300

    using templates

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello),
+    path('', views.index),
     path('about/', views.about),
-    path('hello/<int:id>', views.hello)
+    path('hello/<int:id>', views.hello),
+    path('projects/', views.projects),
+    path('tasks/', views.tasks),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404, render
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Index page")
+    title = 'Django Practice!!'
+    return render(request, 'index.html',{
+        'title': title
+    })
+
+
+def about(request):
+    username = 'lucas'
+    return render(request, 'about.html', {
+        'username': username
+    })
+
 
 def hello(request, id):
     result = id + 2 * 5
-    
     return HttpResponse(f'<h1>Hello {result}</h1>')
 
-def about(request):
-    return HttpResponse('<h2>About</h2>')
+def projects(request):
+    #projects = list(Project.objects.values())
+    projects = Project.objects.all()
+    #return JsonResponse(projects, safe=False)
+    return render(request, 'projects.html', {'projects' : projects})
+
+def tasks(request):
+    #task = get_object_or_404(Task, id=id)
+    #task = Task.objects.get(title=title)
+    #return HttpResponse(f'Task: {task.title}')
+    return render(request, 'tasks.html')
