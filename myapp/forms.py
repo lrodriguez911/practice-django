e1f959ef2a137053aaf9f8df2997836d9b6f97a9
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2023-02-03 20:03:12 -0300

    adding styles and new features like select in form create task

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,5 +1,10 @@
 from django import forms
+from .models import Project
 
 class CreateNewTask(forms.Form):
-    title = forms.CharField(label="Title of task", max_length=200)
-    description = forms.CharField(label="Description of the taks", widget=forms.Textarea)
+    title = forms.CharField(label="Title of task", max_length=200, widget=forms.TextInput(attrs={'class' : 'input'}))
+    description = forms.CharField(label="Description of the taks", widget=forms.Textarea(attrs={'class': 'input'}))
+    project_id = forms.ModelChoiceField(queryset=Project.objects.all(), required=True)
+
+class CreateNewForm(forms.Form):
+    name = forms.CharField(label="Name of Project", max_length=200, widget=forms.TextInput(attrs={'class': 'input'}))
--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,14 @@
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
-from django.shortcuts import get_object_or_404, render
-from .forms import CreateNewTask
+from django.shortcuts import get_object_or_404, render, redirect
+from .forms import CreateNewTask, CreateNewForm
 
 # Create your views here.
 
+
 def index(request):
     title = 'Django Practice!!'
-    return render(request, 'index.html',{
+    return render(request, 'index.html', {
         'title': title
     })
 
@@ -23,22 +24,47 @@ def hello(request, id):
     result = id + 2 * 5
     return HttpResponse(f'<h1>Hello {result}</h1>')
 
+
 def projects(request):
-    #projects = list(Project.objects.values())
+    # projects = list(Project.objects.values())
     projects = Project.objects.all()
-    #return JsonResponse(projects, safe=False)
-    return render(request, 'projects.html', {'projects' : projects})
+    # return JsonResponse(projects, safe=False)
+    return render(request, 'projects/projects.html', {'projects': projects})
+
 
 def tasks(request):
-    #task = get_object_or_404(Task, id=id)
+    # task = get_object_or_404(Task, id=id)
     tasks = Task.objects.all()
-    #return HttpResponse(f'Task: {task.title}')
-    return render(request, 'tasks.html', {"tasks": tasks})
+    # return HttpResponse(f'Task: {task.title}')
+    return render(request, 'tasks/tasks.html', {"tasks": tasks})
+
 
 def create_task(request):
-    print(request.GET['title'])
-    print(request.GET['description'])
-    Task.objects.create(title=request.GET['title'], description=request.GET['description'], project_id=1)
-    return render(request, 'create_task.html', {
-        'form': CreateNewTask
+    if request.method == 'GET':
+        # show interface
+        return render(request, 'tasks/create_task.html', {
+            'form': CreateNewTask
+        })
+    else:
+        print(request.POST['project_id'])
+        Task.objects.create(
+            title=request.POST['title'], description=request.POST['description'], project_id=request.POST['project_id'])
+        return redirect('tasks')
+
+
+def create_project(request):
+    if request.method == 'GET':
+        return render(request, 'projects/create_project.html', {
+            'form': CreateNewForm
+        })
+    else:
+        Project.objects.create(name=request.POST['name'])
+        return redirect('projects')
+
+def project_detail(request, id):
+    project = get_object_or_404(Project, id=id)
+    tasks = Task.objects.filter(project_id=id)
+    return render(request, 'projects/detail.html', {
+        'project' : project,
+        'tasks' : tasks
     })
